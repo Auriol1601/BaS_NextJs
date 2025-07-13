60cc7b895a740d3f02e4d8778b7459af6e8e29f1
auriol1601 <auriol1601@gmail.com>
2025-07-13 14:46:28 +0000

    feat(compoments/Books.jsx):point d'arrivée pour AddBook étape 2/2

---
+++ .history/my-app/app/components/AddBook_20250713144455.jsx
@@ -0,0 +1,56 @@
+'use client' ;
+import { useState } from 'react' ;
+import Link from "next/link" ;
+
+
+const Addbook= ({refreshBooks}) => {
+const [modalOpen, setModalOpen]= useState(false) ;
+const [newBookTitle, setNewBookTitle] = useState('') ;
+const [newBookLink, setNewBookLink] = useState('') ;
+const [newBookImage, setNewBookImage] = useState('') ;
+const handleSubmitNewBook = async (e) =>{
+e.preventDefault() ;
+    const res = await fetch(`/api/books/`,{
+            method : 'POST',
+    headers:{
+        'Content-type' : 'application/json'
+    },
+    body : JSON.stringify({
+        title : newBookTitle,
+        link : " https://www.amazon.com/dp/B0979MGJ5J",
+        img : "https://via.placeholder.com/600/92c952"
+    })
+    })
+    if(res.ok){
+    setNewBookTitle('') ;
+     setModalOpen(false);
+     refreshBooks() ;
+    }
+}
+return (
+    <div>
+        <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>Ajouter Livre </button>
+        <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+            <h3 className="font-bold text-lg">Add New Book</h3>
+            <div className="modal-action">
+            <form method="dialog" onSubmit={handleSubmitNewBook}>
+                <button className="btn">Close</button>
+                <h3 className="font-bold text-lg">Ajouter un nouveau livre</h3>
+                <input type="text"
+                value={newBookTitle}
+                onChange={e=> setNewBookTitle(e.target.value)}
+                placeholder="Entrez le titre du nouveau livre"
+                className="input input-bordered w-full max-w-xs"
+                />
+                <button type="submit" className='btn btn-primary'> 
+                    Ajouter un livre
+                </button>
+            </form>
+            </div>
+        </div>
+        </dialog>
+    </div>
+    )
+}
+export default Addbook ;
---
+++ .history/my-app/app/components/Books_20250713143927.jsx
@@ -0,0 +1,77 @@
+'use client' ;
+import Link from "next/link" ;
+import { useState, useEffect } from "react" ;
+import Loadingpage from "../laoding";
+import Addbook from './AddBook' ;
+
+
+async function getBooks() {
+        const res= await fetch("http://localhost:3000/api/books") ;
+        const json = await res.json() ; 
+        return json ;
+    }
+
+        const Books = () => { 
+        const [books, setBooks]= useState([]) ;
+        const [loading, setLoading] = useState(true) ;
+        const [query, setQuery]= useState("") ;
+        const fetchBooks= async ()=> {
+        const res= await fetch("/api/books") ;
+        const books = await res.json() ;
+        setBooks(books) ; setLoading(false) ;
+        }
+        useEffect(() => {
+            fetchBooks() ;
+        }, []) ;
+        if (loading){ return <Loadingpage />}
+        const handleSubmit = async (e) => {
+        e.preventDefault() ;
+        setLoading(true) ;
+        const res = await fetch(`/api/books/search?query=${query}`) ; 
+        const books = await res.json() ;
+        setBooks(books) ;
+        setLoading(false) ;
+        }
+        return(
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                    type="text"
+                    placeholder="Rechercher des livres..."
+                    value={query} onChange={(e) => setQuery(e.target.value)}
+                    className="input input-bordered w-full max-w-xs"
+                    />
+                    <button type="submit" className="btn btn-primary">
+                        Recherche
+                    </button>
+                </form>
+                <Addbook refreshBooks={fetchBooks}/>
+                <h1>Livres</h1>
+                {books.map((book)=> (
+                <div key={book.id}>
+
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure>
+                            <img src={book.img} width="200" height="150" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{book.id}</h2>
+                            <p>{book.title}</p> 
+                            <p>La carte est un document de travail</p>
+                            <div className="card-actions justify-end">
+                                <Link href={book.link} className="btn btn-primary">Voir sur
+                                Amazon</Link>
+                                <button className="btn btn-error">
+                                Supprimer
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                </div>
+                ))}
+
+            </div>
+    )
+}
+export default Books ;
