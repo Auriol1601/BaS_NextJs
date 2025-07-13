86e20e2a8e6452d5ab30612f7ad3ad26dbbdced7
auriol1601 <auriol1601@gmail.com>
2025-07-13 08:35:57 +0000

    feat(componets/Books.jsx):Trasformer composant serveur en composant client

---
+++ .history/my-app/app/components/Books_20250713082128.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link" ; 
+'use client' ;
+import { useState, useEffect } from "react" ;
+
+async function getBooks() {
+        const res= await fetch("http://localhost:3000/api/books") ;
+        const json = await res.json() ; 
+        return json ;
+    }
+        const Books = async () => { const
+        books = await getBooks() ; 
+        return(
+            <div>
+                <h1>Livres</h1>
+                {books.map((book)=> (
+                <div key={book.id}>
+
+                    <div className="card w-96 bg-base-100 shadow-xl">
+                        <figure>
+                            <img src={book.img} width="200" height="150" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{book.id}</h2>
+                            <p>{book.title}</p> 
+                            <p>La carte est un document de travail</p>
+                            <div className="card-actions justify-end">
+                                <Link href={book.link} className="btn btn-primary">Voir sur
+                                Amazon</Link>
+                                <button className="btn btn-error">
+                                Supprimer
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                </div>
+                ))}
+
+            </div>
+    )
+}
+export default Books ;
