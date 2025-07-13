6da4a4c5b10dc8708faa6dc8d9c55f76ba4ef37d
auriol1601 <auriol1601@gmail.com>
2025-07-13 14:24:02 +0000

    feat(compoments/addBook):point d'arrivée pour AddBook étape 1/2

---
+++ .history/my-app/app/api/books/route_20250713142047.js
@@ -0,0 +1,21 @@
+import books from './data.json' ;
+import { NextResponse } from 'next/server' ;
+import { v4 as uuidv4 } from 'uuid' ;
+
+
+export async function GET(req) { 
+    return NextResponse.json(books) ;
+}
+
+export async function POST(req) {
+    const { title, link, img }= await req.json() ;
+
+    const newBook= {
+        id : uuidv4(),
+        title,
+        link,
+        img
+    };
+    books.push(newBook) ;
+    return NextResponse.json('Livre ajouté avec succès') ;
+}
---
+++ .history/my-app/app/components/AddBook_20250713124805.jsx
@@ -0,0 +1,31 @@
+'use client' ;
+import { useState } from 'react' ;
+import Link from "next/link" ;
+
+const Addbook= ()=> {
+const [modalOpen, setModalOpen]= useState(false) ;
+return (
+    <div>
+        <button className="btn" onClick={()=>document.getElementById('my_modal_1').showModal()}>Ajouter Livre </button>
+        <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+            <h3 className="font-bold text-lg">Add New Book</h3>
+            <div className="modal-action">
+            <form method="dialog">
+                <button className="btn">Close</button>
+                <h3 className="font-bold text-lg">Ajouter un nouveau livre</h3>
+                <input type="text"
+                placeholder="Entrez le titre du nouveau livre"
+                className="input input-bordered w-full max-w-xs"
+                />
+                <button type="submit" className='btn btn-primary'> Ajouter un
+                livre
+                </button>
+            </form>
+            </div>
+        </div>
+        </dialog>
+    </div>
+    )
+}
+export default Addbook ;
